c498a2decb4b52fe362595a5ba35d71440be2a42
asunder2 <87463942+asunder2@users.noreply.github.com>
2024-03-25 16:42:04 -0700

    Add multiply

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -194,28 +194,29 @@ class WebServer {
             builder.append("File not found: " + file);
           }
         } else if (request.contains("multiply?")) {
-          // This multiplies two numbers, there is NO error handling, so when
-          // wrong data is given this just crashes
-
           Map<String, String> query_pairs = new LinkedHashMap<String, String>();
-          // extract path parameters
           query_pairs = splitQuery(request.replace("multiply?", ""));
-
-          // extract required fields from parameters
-          Integer num1 = Integer.parseInt(query_pairs.get("num1"));
-          Integer num2 = Integer.parseInt(query_pairs.get("num2"));
-
-          // do math
-          Integer result = num1 * num2;
-
-          // Generate response
-          builder.append("HTTP/1.1 200 OK\n");
-          builder.append("Content-Type: text/html; charset=utf-8\n");
-          builder.append("\n");
-          builder.append("Result is: " + result);
-
-          // TODO: Include error handling here with a correct error code and
-          // a response that makes sense
+          if (query_pairs.containsKey("num1") && query_pairs.containsKey("num2")) {
+            try {
+              Integer num1 = Integer.parseInt(query_pairs.get("num1"));
+              Integer num2 = Integer.parseInt(query_pairs.get("num2"));
+              Integer result = num1 * num2;
+              builder.append("HTTP/1.1 200 OK\\n");
+              builder.append("Content-Type: text/html; charset=utf-8\\n");
+              builder.append("\\n");
+              builder.append("Result is: " + result);
+            } catch (NumberFormatException e) {
+              builder.append("HTTP/1.1 400 Bad Request\\n");
+              builder.append("Content-Type: text/html; charset=utf-8\\n");
+              builder.append("\\n");
+              builder.append("Error: One or both parameters are not valid integers.");
+            }
+          } else {
+            builder.append("HTTP/1.1 400 Bad Request\\n");
+            builder.append("Content-Type: text/html; charset=utf-8\\n");
+            builder.append("\\n");
+            builder.append("Error: One or both parameters are missing.");
+          }
 
         } else if (request.contains("github?")) {
           // pulls the query from the request and runs it with GitHub's REST API
